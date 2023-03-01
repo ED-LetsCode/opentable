102e4749dafa1e9cd231d9f6a9d39d774cb14786
Eray <eray.demirel@lets-code.at>
2023-03-01 18:30:43 +0100

    update

--- src/app/components/appComponents/Components.tsx
+++ src/app/components/appComponents/Components.tsx
@@ -4,5 +4,16 @@ import RestaurantCard from "./RestaurantCard";
 import SearchBar from "./SearchBar";
 import Price from "./Price";
 import Stars from "./Stars";
+import AuthModal from "./AuthModal";
+import AuthModalInputs from "./AuthModalInputs";
 
-export { Header, Navbar, RestaurantCard, SearchBar, Price, Stars };
+export {
+  Header,
+  Navbar,
+  RestaurantCard,
+  SearchBar,
+  Price,
+  Stars,
+  AuthModal,
+  AuthModalInputs,
+};
